e1793f6dca1e1c67146e6942555cd2eb2516358e
Jahid <jahid90@users.noreply.github.com>
2021-05-21 22:55:16 +0530

    add layout for counter widget

--- src/server.js
+++ src/server.js
@@ -54,6 +54,25 @@ const layouts = {
             }
         ]
     },
+    '3': {
+        id: 3,
+        title: 'Counter',
+        layout: 'single-column-with-nav',
+        slots: [
+            {
+                id: 'nav',
+                mode: 'atf'
+            },
+            {
+                id: '1',
+                mode: 'atf'
+            },
+            {
+                id: 'footer',
+                mode: 'atf'
+            }
+        ]
+    },
     '404': {
         id: 404,
         title: 'Not Found',
