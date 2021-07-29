0fc1dafb757be0d21ca32533025f6c12396399be
Jahid <jahid90@users.noreply.github.com>
2021-07-29 19:00:30 +0530

    externalize configuration

---
+++ src/config/layouts.js
@@ -0,0 +1,85 @@
+module.exports = {
+    '1': {
+        id: 1,
+        title: 'Home',
+        layout: 'single-column-with-nav',
+        slots: [
+            {
+                id: 'nav',
+            },
+            {
+                id: '1'
+            },
+            {
+                id: '2',
+            },
+            {
+                id: 'footer'
+            }
+        ]
+    },
+    '2': {
+        id: 2,
+        title: 'About',
+        layout: 'single-column-with-nav',
+        slots: [
+            {
+                id: 'nav'
+            },
+            {
+                id: '1',
+            },
+            {
+                id: 'footer'
+            }
+        ]
+    },
+    '3': {
+        id: 3,
+        title: 'Minance App',
+        layout: 'single-column-with-nav',
+        slots: [
+            {
+                id: 'nav'
+            },
+            {
+                id: '1',
+            },
+            {
+                id: 'footer'
+            }
+        ]
+    },
+    '4': {
+        id: 4,
+        title: 'Todos App',
+        layout: 'single-column-with-nav',
+        slots: [
+            {
+                id: 'nav'
+            },
+            {
+                id: '1'
+            },
+            {
+                id: 'footer'
+            }
+        ]
+    },
+    '404': {
+        id: 404,
+        title: 'Not Found',
+        layout: 'single-column-with-nav',
+        slots: [
+            {
+                id: 'nav'
+            },
+            {
+                id: '1'
+            },
+            {
+                id: 'footer'
+            }
+        ]
+    }
+}
--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const morgan = require('morgan');
 
+const layouts = require('./config/layouts');
+
 // Create the server
 const app = express();
 
@@ -10,92 +12,6 @@ app.use(morgan('tiny'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-const layouts = {
-    '1': {
-        id: 1,
-        title: 'Home',
-        layout: 'single-column-with-nav',
-        slots: [
-            {
-                id: 'nav',
-            },
-            {
-                id: '1'
-            },
-            {
-                id: '2',
-            },
-            {
-                id: 'footer'
-            }
-        ]
-    },
-    '2': {
-        id: 2,
-        title: 'About',
-        layout: 'single-column-with-nav',
-        slots: [
-            {
-                id: 'nav'
-            },
-            {
-                id: '1',
-            },
-            {
-                id: 'footer'
-            }
-        ]
-    },
-    '3': {
-        id: 3,
-        title: 'Minance App',
-        layout: 'single-column-with-nav',
-        slots: [
-            {
-                id: 'nav'
-            },
-            {
-                id: '1',
-            },
-            {
-                id: 'footer'
-            }
-        ]
-    },
-    '4': {
-        id: 4,
-        title: 'Todos App',
-        layout: 'single-column-with-nav',
-        slots: [
-            {
-                id: 'nav'
-            },
-            {
-                id: '1'
-            },
-            {
-                id: 'footer'
-            }
-        ]
-    },
-    '404': {
-        id: 404,
-        title: 'Not Found',
-        layout: 'single-column-with-nav',
-        slots: [
-            {
-                id: 'nav'
-            },
-            {
-                id: '1'
-            },
-            {
-                id: 'footer'
-            }
-        ]
-    }
-}
 
 // Routes
 app.post('/', (req, res) => {
